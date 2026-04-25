7c12ea90d2b625f0b30a89372cd988423a2e93af
gustavonunespaz <gust.nunes.paz@gmail.com>
2026-04-25 18:24:09 -0300

    Implementei a nova identidade visual da SoIZI na landing page, com uma estrutura mais moderna, seções informativas sobre a plataforma e ajustes de configuração para suporte a imagens externas

--- calculadora-nutricional/next.config.ts
+++ calculadora-nutricional/next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "unavatar.io",
+      },
+      {
+        protocol: "https",
+        hostname: "media.licdn.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
